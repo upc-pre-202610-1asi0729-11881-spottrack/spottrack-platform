3d32d7b560def6fe37510cc84345346eb770dbda
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-02 12:46:49 -0500

    feat: add lists to each entity in equipment bounded context

--- src/main/java/com/spottrack/platform/equipment/domain/model/aggregates/Gym.java
+++ src/main/java/com/spottrack/platform/equipment/domain/model/aggregates/Gym.java
@@ -1,10 +1,14 @@
 package com.spottrack.platform.equipment.domain.model.aggregates;
 
+import com.spottrack.platform.equipment.domain.model.entities.Branch;
 import com.spottrack.platform.equipment.domain.model.valueobjects.GymId;
 import com.spottrack.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -14,10 +18,14 @@ public class Gym extends AbstractDomainAggregateRoot<Gym> {
     private GymId id;
     private String name;
 
+    @ElementCollection
+    private List<Branch> branchList;
+
     protected Gym() {}
 
     public Gym(String name) {
         this.id = new GymId(UUID.randomUUID().toString());
         this.name = name;
+        this.branchList = new ArrayList<Branch>();
     }
 }
