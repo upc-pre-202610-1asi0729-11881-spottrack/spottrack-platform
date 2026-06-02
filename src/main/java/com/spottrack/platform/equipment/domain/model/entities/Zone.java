fa095d5a3cea932fac957b6468979db90a3782df
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-02 12:22:46 -0500

    feat: add zone id

---
+++ src/main/java/com/spottrack/platform/equipment/domain/model/entities/Zone.java
@@ -0,0 +1,12 @@
+package com.spottrack.platform.equipment.domain.model.entities;
+
+public class Zone {
+    String name;
+    /**
+     * Capacity limit here
+     * |
+     * v
+     */
+    int maximumOccupancy;
+
+}
