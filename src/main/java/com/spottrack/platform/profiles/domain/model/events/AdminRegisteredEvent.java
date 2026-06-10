256814466980b36669d0aeb50b3ee665a32ca66a
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-09 19:25:10 -0500

    domain events setted

--- src/main/java/com/spottrack/platform/profiles/domain/model/events/AdminRegisteredEvent.java
+++ src/main/java/com/spottrack/platform/profiles/domain/model/events/AdminRegisteredEvent.java
@@ -1,4 +1,24 @@
 package com.spottrack.platform.profiles.domain.model.events;
 
-public class AdminRegisteredEvent {
+import com.spottrack.platform.profiles.domain.model.aggregates.Admin;
+
+public record AdminRegisteredEvent(
+        Long adminId,
+        String firstName,
+        String lastName,
+        String email,
+        String phoneNumber,
+        String dni
+) {
+    public static AdminRegisteredEvent from(Admin admin) {
+        var info = admin.getPersonInfo();
+        return new AdminRegisteredEvent(
+                admin.getId(),
+                info.firstName(),
+                info.lastName(),
+                admin.getEmailAddress(),
+                info.phoneNumber().phoneNumber(),
+                info.dni().dni()
+        );
+    }
 }
--- src/main/java/com/spottrack/platform/profiles/domain/model/events/ClientRegisteredEvent.java
+++ src/main/java/com/spottrack/platform/profiles/domain/model/events/ClientRegisteredEvent.java
@@ -1,4 +1,24 @@
 package com.spottrack.platform.profiles.domain.model.events;
 
-public class ClientRegisteredEvent {
+import com.spottrack.platform.profiles.domain.model.aggregates.Client;
+
+public record ClientRegisteredEvent(
+        Long clientId,
+        String firstName,
+        String lastName,
+        String email,
+        String phoneNumber,
+        String dni
+) {
+    public static ClientRegisteredEvent from(Client client) {
+        var info = client.getPersonInfo();
+        return new ClientRegisteredEvent(
+                client.getId(),
+                info.firstName(),
+                info.lastName(),
+                client.getEmailAddress(),
+                info.phoneNumber().phoneNumber(),
+                info.dni().dni()
+        );
+    }
 }
