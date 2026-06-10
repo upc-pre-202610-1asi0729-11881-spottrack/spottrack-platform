fe4e01ea5e22a16d6207c3184d5f6d7539dd9446
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-09 20:50:54 -0500

    domain layer added

--- src/main/java/com/spottrack/platform/profiles/domain/model/aggregates/Client.java
+++ src/main/java/com/spottrack/platform/profiles/domain/model/aggregates/Client.java
@@ -1,4 +1,57 @@
 package com.spottrack.platform.profiles.domain.model.aggregates;
 
-public class Client {
+import com.spottrack.platform.profiles.domain.model.commands.CreateClientCommand;
+import com.spottrack.platform.profiles.domain.model.commands.UpdateClientProfileCommand;
+import com.spottrack.platform.profiles.domain.model.events.ClientRegisteredEvent;
+import com.spottrack.platform.profiles.domain.model.valueobjects.EmailAddress;
+import com.spottrack.platform.profiles.domain.model.valueobjects.PersonInfo;
+import com.spottrack.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+public class Client extends AbstractDomainAggregateRoot<Client> {
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    private Long userId;
+
+    @Getter
+    private PersonInfo personInfo;
+
+    private EmailAddress emailAddress;
+
+    public Client(Long id, Long userId, PersonInfo personInfo, EmailAddress emailAddress){
+        this.id = id;
+        this.userId = userId;
+        this.personInfo = Objects.requireNonNull(personInfo, "PersonInfo must not be null");
+        this.emailAddress = Objects.requireNonNull(emailAddress,"Email must not be null");
+    }
+
+    public Client(CreateClientCommand command){
+        this(
+                null,
+                command.userId(),
+                new PersonInfo(command.firstName(), command.lastName(), command.phoneNumber(), command.dni()),
+                command.emailAddress()
+        );
+    }
+
+    public void updateProfile(UpdateClientProfileCommand command) {
+        this.personInfo = new PersonInfo(command.firstName(),command.lastName(),command.phoneNumber(), this.personInfo.dni());
+    }
+
+    public PersonInfo getPersonInfo(){return personInfo;}
+
+    public String getFullName(){return personInfo.getFullName();}
+
+    public String getEmailAddress() {return emailAddress.address();}
+
+    public void onCreated() {
+        registerDomainEvent(ClientRegisteredEvent.from(this));
+    }
+
 }
