d71ba7a3ac4e9df7fb2b9bc9c16c6e34c03595ce
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-12 12:04:38 -0500

    admin queries validation

--- src/main/java/com/spottrack/platform/profiles/domain/model/queries/GetAdminByIdQuery.java
+++ src/main/java/com/spottrack/platform/profiles/domain/model/queries/GetAdminByIdQuery.java
@@ -3,4 +3,9 @@ package com.spottrack.platform.profiles.domain.model.queries;
 import com.spottrack.platform.profiles.domain.model.valueobjects.AdminId;
 
 public record GetAdminByIdQuery (AdminId adminId){
+    public GetAdminByIdQuery{
+        if (adminId == null || adminId.adminId() <= 0){
+            throw new IllegalArgumentException("Admin Id cannot be null or less than or equal to 0");
+        }
+    }
 }
