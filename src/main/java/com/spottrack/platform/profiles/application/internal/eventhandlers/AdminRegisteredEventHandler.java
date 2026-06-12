7d8d1b4a4e18e6c53ab599e14f23bbf4761c29f1
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-12 11:40:12 -0500

    Interfaces new integration events setted

---
+++ src/main/java/com/spottrack/platform/profiles/application/internal/eventhandlers/AdminRegisteredEventHandler.java
@@ -0,0 +1,23 @@
+package com.spottrack.platform.profiles.application.internal.eventhandlers;
+
+import com.spottrack.platform.profiles.domain.model.events.AdminRegisteredEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AdminRegisteredEventHandler {
+    private final ApplicationEventPublisher eventPublisher;
+
+    public AdminRegisteredEventHandler(ApplicationEventPublisher eventPublisher){
+        this.eventPublisher = eventPublisher;
+    }
+
+    @EventListener
+    public void on(AdminRegisteredEvent event){
+
+    }
+
+}
---
+++ src/main/java/com/spottrack/platform/profiles/application/internal/eventhandlers/ClientRegisteredEventHandler.java
@@ -0,0 +1,4 @@
+package com.spottrack.platform.profiles.application.internal.eventhandlers;
+
+public class ClientRegisteredEventHandler {
+}
