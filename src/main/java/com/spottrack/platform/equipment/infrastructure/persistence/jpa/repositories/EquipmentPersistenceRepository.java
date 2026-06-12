c53951762bb9e314d11dd54cc317a3ab3daa52ab
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 11:08:42 -0500

    feat(equipments): finished equipment persistence repository

--- src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/repositories/EquipmentPersistenceRepository.java
+++ src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/repositories/EquipmentPersistenceRepository.java
@@ -2,12 +2,14 @@ package com.spottrack.platform.equipment.infrastructure.persistence.jpa.reposito
 
 import com.spottrack.platform.equipment.domain.model.aggregates.Equipment;
 import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentId;
+import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentStatus;
 import com.spottrack.platform.equipment.infrastructure.persistence.jpa.entities.EquipmentPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EquipmentPersistenceRepository extends JpaRepository<EquipmentPersistenceEntity, Long> {
-    Optional<EquipmentPersistenceEntity> findByEquipmentId(Strinb equipmentId);
-
+    Optional<EquipmentPersistenceEntity> findByEquipmentId(String equipmentId);
+    Optional<EquipmentPersistenceEntity> findByEquipmentName(String equipmentName);
+    Optional<EquipmentStatus> findByStatus(String equipmentStatus);
 }
