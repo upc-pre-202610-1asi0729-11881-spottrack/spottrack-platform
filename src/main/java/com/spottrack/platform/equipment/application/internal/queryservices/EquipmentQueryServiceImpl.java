a91edfe848d4e95e971c7893ce530303d21f34a0
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 14:48:33 -0500

    feat(equipments): add getEquipments query

--- src/main/java/com/spottrack/platform/equipment/application/internal/queryservices/EquipmentQueryServiceImpl.java
+++ src/main/java/com/spottrack/platform/equipment/application/internal/queryservices/EquipmentQueryServiceImpl.java
@@ -1,4 +1,47 @@
 package com.spottrack.platform.equipment.application.internal.queryservices;
 
-public class EquipmentQueryServiceImpl {
+import com.spottrack.platform.equipment.application.queryservices.EquipmentQueryService;
+import com.spottrack.platform.equipment.domain.model.aggregates.Equipment;
+import com.spottrack.platform.equipment.domain.model.queries.GetEquipmentStatus;
+import com.spottrack.platform.equipment.domain.model.queries.GetEquipments;
+import com.spottrack.platform.equipment.domain.model.queries.GetEquipmentById;
+import com.spottrack.platform.equipment.domain.model.queries.GetEquipmentByName;
+import com.spottrack.platform.equipment.infrastructure.persistence.jpa.assemblers.EquipmentPersistenceAssembler;
+import com.spottrack.platform.equipment.infrastructure.persistence.jpa.entities.EquipmentPersistenceEntity;
+import com.spottrack.platform.equipment.infrastructure.persistence.jpa.repositories.EquipmentPersistenceRepository;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EquipmentQueryServiceImpl implements EquipmentQueryService {
+    EquipmentPersistenceRepository equipmentPersistenceRepository;
+
+    public EquipmentQueryServiceImpl(EquipmentPersistenceRepository equipmentPersistenceRepository) {
+        this.equipmentPersistenceRepository = equipmentPersistenceRepository;
+    }
+
+    @Override
+    public Optional<Equipment> handle(GetEquipmentById query) {
+        return Optional.of();
+    }
+
+    @Override
+    public Optional<Equipment> handle(GetEquipmentByName query) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Equipment> handle(GetEquipmentStatus query) {
+        return List.of();
+    }
+
+    @Override
+    public List<Equipment> handle(GetEquipments query) {
+        var persistenceEquipment = equipmentPersistenceRepository.findAll();
+        var persistenceToDomain = persistenceEquipment.stream().map(EquipmentPersistenceAssembler::toDomainFromPersistence);
+        return persistenceToDomain.toList();
+    }
 }
--- src/main/java/com/spottrack/platform/equipment/domain/model/queries/GetEquipmentById.java
+++ src/main/java/com/spottrack/platform/equipment/domain/model/queries/GetEquipmentById.java
@@ -2,8 +2,8 @@ package com.spottrack.platform.equipment.domain.model.queries;
 
 import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentId;
 
-public record GetEquipmentsById(EquipmentId id) {
-    public GetEquipmentsById {
+public record GetEquipmentById(EquipmentId id) {
+    public GetEquipmentById {
         if (id == null){
             throw new IllegalArgumentException("id must not be null");
         }
