5858d6a70d2fbb44c7f4d7bfebe4cefab6a9f139
alumnos <u20231146@upc.edu.pe>
2026-06-12 08:12:12 -0500

    feat(equipment): add decommissionEquipment command

--- src/main/java/com/spottrack/platform/equipment/domain/model/commands/DecomissionEquipment.java
+++ src/main/java/com/spottrack/platform/equipment/domain/model/commands/DecomissionEquipment.java
@@ -1,11 +1,24 @@
 package com.spottrack.platform.equipment.domain.model.commands;
 
 import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentId;
+import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentStatus;
 
 /**
- * Dont understand quite right what this should do
+ * Changes the state of an equipment to decommissioned
  */
 
 public record DecomissionEquipment(
+        EquipmentId equipmentId, EquipmentStatus equipmentStatus
 ) {
+    public DecomissionEquipment {
+        if (equipmentId == null){
+            throw new IllegalArgumentException("equipmentId must not be null");
+        }
+
+        if (equipmentStatus == null){
+            throw new IllegalArgumentException("equipmentStatus must not be null");
+
+        }
+    }
+
 }
