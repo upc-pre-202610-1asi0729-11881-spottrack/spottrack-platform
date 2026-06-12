56697aeaf16b11cc10e5a42a556b82346f4f4b11
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-12 11:42:56 -0500

    AdminEventHandlerSetted

--- src/main/java/com/spottrack/platform/profiles/application/internal/eventhandlers/AdminRegisteredEventHandler.java
+++ src/main/java/com/spottrack/platform/profiles/application/internal/eventhandlers/AdminRegisteredEventHandler.java
@@ -1,6 +1,7 @@
 package com.spottrack.platform.profiles.application.internal.eventhandlers;
 
 import com.spottrack.platform.profiles.domain.model.events.AdminRegisteredEvent;
+import com.spottrack.platform.profiles.interfaces.events.AdminRegisteredIntegrationEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
@@ -17,7 +18,12 @@ public class AdminRegisteredEventHandler {
 
     @EventListener
     public void on(AdminRegisteredEvent event){
-
+        eventPublisher.publishEvent(new AdminRegisteredIntegrationEvent(
+                event.adminId(),
+                null,
+                event.firstName(),
+                event.lastName(),
+                event.email()
+        ));
     }
-
 }
