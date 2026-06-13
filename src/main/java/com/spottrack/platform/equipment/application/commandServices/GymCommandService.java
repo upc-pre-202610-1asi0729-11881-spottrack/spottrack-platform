13020fa60c969fbe245cc691a25c4b5b1d2f87d9
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 23:29:10 -0500

    feat(equipments): add gym command service

---
+++ src/main/java/com/spottrack/platform/equipment/application/commandServices/GymCommandService.java
@@ -0,0 +1,10 @@
+package com.spottrack.platform.equipment.application.commandServices;
+
+import com.spottrack.platform.equipment.domain.model.aggregates.Equipment;
+import com.spottrack.platform.equipment.domain.model.commands.RelocateEquipment;
+import com.spottrack.platform.shared.application.result.ApplicationError;
+import com.spottrack.platform.shared.application.result.Result;
+
+public interface GymCommandService {
+    public Result<Equipment, ApplicationError> handle(RelocateEquipment command);
+}
