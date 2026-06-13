4505c7c1cc90a6743c486546fb74007ffd9e5216
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 23:34:43 -0500

    feat(equipments): add request equipment relocation command service

--- src/main/java/com/spottrack/platform/equipment/application/commandServices/GymCommandService.java
+++ src/main/java/com/spottrack/platform/equipment/application/commandServices/GymCommandService.java
@@ -2,9 +2,11 @@ package com.spottrack.platform.equipment.application.commandServices;
 
 import com.spottrack.platform.equipment.domain.model.aggregates.Equipment;
 import com.spottrack.platform.equipment.domain.model.commands.RelocateEquipment;
+import com.spottrack.platform.equipment.domain.model.commands.RequestEquipmentRelocation;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 import com.spottrack.platform.shared.application.result.Result;
 
 public interface GymCommandService {
     public Result<Equipment, ApplicationError> handle(RelocateEquipment command);
+    public Result<Equipment, ApplicationError> handle(RequestEquipmentRelocation command);
 }
--- src/main/java/com/spottrack/platform/equipment/domain/model/commands/RequestEquipmentRelocation.java
+++ src/main/java/com/spottrack/platform/equipment/domain/model/commands/RequestEquipmentRelocation.java
@@ -3,8 +3,14 @@ package com.spottrack.platform.equipment.domain.model.commands;
 import com.spottrack.platform.equipment.domain.model.valueobjects.EquipmentId;
 import com.spottrack.platform.equipment.domain.model.valueobjects.ZoneId;
 
-public record RequestRelocateEquipment(EquipmentId equipmentId, ZoneId zoneId) {
-    public RequestRelocateEquipment{
+/**
+ * This should talk to an alert service, different from the actual relocate command, this one just
+ * notifies that it might be convenient to relocate an equipment
+ * @param equipmentId
+ * @param zoneId
+ */
+public record RequestEquipmentRelocation(EquipmentId equipmentId, ZoneId zoneId) {
+    public RequestEquipmentRelocation {
         if (equipmentId == null){
             throw new IllegalArgumentException("equipmentId must not be null");
         }
