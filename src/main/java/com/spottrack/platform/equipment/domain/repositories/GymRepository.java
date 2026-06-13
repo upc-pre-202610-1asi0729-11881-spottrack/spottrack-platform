9b4f51c7b8f31f50a67022c583fa83c56100b032
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 23:26:46 -0500

    feat(equipments): add functions to gym repositories

--- src/main/java/com/spottrack/platform/equipment/domain/repositories/GymRepository.java
+++ src/main/java/com/spottrack/platform/equipment/domain/repositories/GymRepository.java
@@ -1,7 +1,9 @@
 package com.spottrack.platform.equipment.domain.repositories;
-
 import com.spottrack.platform.equipment.domain.model.aggregates.Gym;
+import com.spottrack.platform.equipment.domain.model.valueobjects.GymId;
 
 public interface GymRepository {
     Gym save(Gym gym);
+    Optional<Gym> findById(GymId gymId);
+
 }
--- src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/repositories/GymPersistenceRepository.java
+++ src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/repositories/GymPersistenceRepository.java
@@ -3,5 +3,8 @@ package com.spottrack.platform.equipment.infrastructure.persistence.jpa.reposito
 import com.spottrack.platform.equipment.infrastructure.persistence.jpa.entities.GymPersistenceEntity;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 
+import java.util.Optional;
+
 public interface GymPersistenceRepository {
+    Optional<GymPersistenceEntity> findByGymId(String gymId);
 }
