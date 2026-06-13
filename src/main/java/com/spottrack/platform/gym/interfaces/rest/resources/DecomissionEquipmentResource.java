3ef70250bc6c5760cb10287be1f11f37dab7f147
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-13 16:21:07 -0500

    feat(gym): add decomission equipment resource

---
+++ src/main/java/com/spottrack/platform/gym/interfaces/rest/resources/DecomissionEquipmentResource.java
@@ -0,0 +1,6 @@
+package com.spottrack.platform.gym.interfaces.rest.resources;
+
+import com.spottrack.platform.gym.domain.model.valueobjects.EquipmentStatus;
+
+public record DecomissionEquipmentResource(String equipmentId, EquipmentStatus status) {
+}
