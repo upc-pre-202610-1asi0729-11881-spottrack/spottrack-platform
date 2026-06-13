51a426ac35d220df6490a743752c08ffcacaef5a
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-12 22:56:39 -0500

    feat(equipments): add gym persistence entity

--- src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/entities/GymPersistenceEntity.java
+++ src/main/java/com/spottrack/platform/equipment/infrastructure/persistence/jpa/entities/GymPersistenceEntity.java
@@ -1,10 +1,15 @@
 package com.spottrack.platform.equipment.infrastructure.persistence.jpa.entities;
 
+import com.spottrack.platform.equipment.domain.model.entities.Branch;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,8 +17,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name="gyms")
 public class GymPersistenceEntity {
-    String gymId;
-    String name;
+    @Column(nullable = false, unique = true)
+    private String gymId;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @OneToMany
+    private List<Branch> branchList;
 
 }
 
