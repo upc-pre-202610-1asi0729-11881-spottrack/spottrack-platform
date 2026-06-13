7ee5e5886fa162038c3508fef583b0ee0766735f
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-13 13:51:47 -0500

    feat(gym): add create gym command to command from resource assembler

--- src/main/java/com/spottrack/platform/gym/domain/model/commands/CreateGym.java
+++ src/main/java/com/spottrack/platform/gym/domain/model/commands/CreateGym.java
@@ -2,11 +2,9 @@ package com.spottrack.platform.gym.domain.model.commands;
 
 import com.spottrack.platform.gym.domain.model.valueobjects.GymId;
 
-public record CreateGym (GymId gymId, String gymName){
+public record CreateGym ( String gymName){
     public CreateGym {
-        if (gymId == null) {
-            throw new IllegalArgumentException("gymId must not be null");
-        }
+
         if (gymName == null || gymName.isBlank()) {
             throw new IllegalArgumentException("gymName must not be null or blank");
         }
