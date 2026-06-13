c7751d51353db8ee1c8a1211f9ed96d3519de8c0
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-13 13:37:13 -0500

    feat(gym): add create gym command and create gym command impl

--- src/main/java/com/spottrack/platform/gym/application/commandServices/GymCommandService.java
+++ src/main/java/com/spottrack/platform/gym/application/commandServices/GymCommandService.java
@@ -1,6 +1,8 @@
 package com.spottrack.platform.gym.application.commandServices;
 
 import com.spottrack.platform.gym.domain.model.aggregates.Equipment;
+import com.spottrack.platform.gym.domain.model.aggregates.Gym;
+import com.spottrack.platform.gym.domain.model.commands.CreateGym;
 import com.spottrack.platform.gym.domain.model.commands.RelocateEquipment;
 import com.spottrack.platform.gym.domain.model.commands.RequestEquipmentRelocation;
 import com.spottrack.platform.shared.application.result.ApplicationError;
@@ -8,4 +10,5 @@ import com.spottrack.platform.shared.application.result.Result;
 
 public interface GymCommandService {
     public Result<Equipment, ApplicationError> handle(RequestEquipmentRelocation command);
+    public Result<Gym, ApplicationError> handle(CreateGym command);
 }
