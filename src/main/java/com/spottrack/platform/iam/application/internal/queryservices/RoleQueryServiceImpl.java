80e08aa420daa3a7250ed737e5110149d701199a
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 23:16:40 -0500

    feat(iam): add RoleQueryServiceImpl

---
+++ src/main/java/com/spottrack/platform/iam/application/internal/queryservices/RoleQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.spottrack.platform.iam.application.internal.queryservices;
+
+import com.spottrack.platform.iam.application.queryservices.RoleQueryService;
+import com.spottrack.platform.iam.domain.model.entities.Role;
+import com.spottrack.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.spottrack.platform.iam.domain.model.queries.GetRoleByNameQuery;
+import com.spottrack.platform.iam.domain.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleQueryServiceImpl implements RoleQueryService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleQueryServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> handle(GetAllRolesQuery query) {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> handle(GetRoleByNameQuery query) {
+        return roleRepository.findByName(query.name());
+    }
+}
