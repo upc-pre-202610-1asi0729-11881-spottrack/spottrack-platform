32c674f1698419d034f73e1a4eb54689cd566e1c
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 21:01:24 -0500

    feat(iam): add UserPersistenceEntity

---
+++ src/main/java/com/spottrack/platform/iam/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
@@ -0,0 +1,49 @@
+package com.spottrack.platform.iam.infrastructure.persistence.jpa.entities;
+
+import com.spottrack.platform.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserPersistenceEntity extends AuditableAbstractPersistenceEntity {
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<RolePersistenceEntity> roles;
+
+    public UserPersistenceEntity() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<RolePersistenceEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RolePersistenceEntity> roles) {
+        this.roles = roles;
+    }
+}
