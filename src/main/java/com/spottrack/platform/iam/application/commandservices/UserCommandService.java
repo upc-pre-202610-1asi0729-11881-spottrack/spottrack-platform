680aa1c57def9c5bbb27e0ed2f5a97e639b7f75f
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 22:41:34 -0500

    feat(iam): add ResetPasswordCommand handler to UserCommandService

--- src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
+++ src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
@@ -1,6 +1,7 @@
 package com.spottrack.platform.iam.application.commandservices;
 
 import com.spottrack.platform.iam.domain.model.aggregates.User;
+import com.spottrack.platform.iam.domain.model.commands.ResetPasswordCommand;
 import com.spottrack.platform.iam.domain.model.commands.SignInCommand;
 import com.spottrack.platform.iam.domain.model.commands.SignUpCommand;
 import com.spottrack.platform.shared.application.result.ApplicationError;
@@ -10,4 +11,5 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 public interface UserCommandService {
     Result<User, ApplicationError> handle(SignUpCommand command);
     Result<ImmutablePair<User, String>, ApplicationError> handle(SignInCommand command);
+    Result<User, ApplicationError> handle(ResetPasswordCommand command);
 }
