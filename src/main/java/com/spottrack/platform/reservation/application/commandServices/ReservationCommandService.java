7d00ea7773dba98e4b407a59ac3caacfacdf8c28
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-16 00:11:06 -0500

    feat(reservation): add reservationRequest domain respository

--- src/main/java/com/spottrack/platform/reservation/application/commandServices/ReservationCommandService.java
+++ src/main/java/com/spottrack/platform/reservation/application/commandServices/ReservationCommandService.java
@@ -21,4 +21,5 @@ public interface ReservationCommandService {
     Result<Reservation, ApplicationError> handle(StartReservationTimer command);
 
     Result<Reservation, ApplicationError> handle(EndReservation command);
+
 }
