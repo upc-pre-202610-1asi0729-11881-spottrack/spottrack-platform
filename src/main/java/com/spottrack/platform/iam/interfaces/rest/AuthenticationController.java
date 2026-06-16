ada69ddf198dc7e8322a0f2a04d40c72070a4e8d
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 22:52:53 -0500

    feat(iam): add sign-out endpoint to AuthenticationController

--- src/main/java/com/spottrack/platform/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/spottrack/platform/iam/interfaces/rest/AuthenticationController.java
@@ -3,10 +3,12 @@ package com.spottrack.platform.iam.interfaces.rest;
 import com.spottrack.platform.iam.application.commandservices.UserCommandService;
 import com.spottrack.platform.iam.interfaces.rest.resources.ResetPasswordResource;
 import com.spottrack.platform.iam.interfaces.rest.resources.SignInResource;
+import com.spottrack.platform.iam.interfaces.rest.resources.SignOutResource;
 import com.spottrack.platform.iam.interfaces.rest.resources.SignUpResource;
 import com.spottrack.platform.iam.interfaces.rest.transform.AuthenticatedUserResourceFromEntityAssembler;
 import com.spottrack.platform.iam.interfaces.rest.transform.ResetPasswordCommandFromResourceAssembler;
 import com.spottrack.platform.iam.interfaces.rest.transform.SignInCommandFromResourceAssembler;
+import com.spottrack.platform.iam.interfaces.rest.transform.SignOutCommandFromResourceAssembler;
 import com.spottrack.platform.iam.interfaces.rest.transform.SignUpCommandFromResourceAssembler;
 import com.spottrack.platform.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
 import com.spottrack.platform.shared.interfaces.rest.transform.ResponseEntityAssembler;
@@ -52,6 +54,17 @@ public class AuthenticationController {
     @PostMapping("/reset-password")
     public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordResource resource) {
         var command = ResetPasswordCommandFromResourceAssembler.toCommandFromResource(resource);
+        var result = userCommandService.handle(command);
+        return ResponseEntityAssembler.toResponseEntityFromResult(
+                result,
+                UserResourceFromEntityAssembler::toResourceFromEntity,
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/sign-out")
+    public ResponseEntity<?> signOut(@Valid @RequestBody SignOutResource resource) {
+        var command = SignOutCommandFromResourceAssembler.toCommandFromResource(resource);
         var result = userCommandService.handle(command);
         return ResponseEntityAssembler.toResponseEntityFromResult(
                 result,
