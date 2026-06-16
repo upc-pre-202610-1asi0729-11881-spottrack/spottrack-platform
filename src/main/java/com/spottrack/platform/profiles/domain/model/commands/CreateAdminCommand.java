a53f02d785c92458756f998151de9004dc645104
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 23:30:57 -0500

    fix(profiles): make phoneNumber and dni raw strings in CreateAdminCommand

--- src/main/java/com/spottrack/platform/profiles/domain/model/commands/CreateAdminCommand.java
+++ src/main/java/com/spottrack/platform/profiles/domain/model/commands/CreateAdminCommand.java
@@ -1,14 +1,12 @@
 package com.spottrack.platform.profiles.domain.model.commands;
 
-import com.spottrack.platform.profiles.domain.model.valueobjects.Dni;
 import com.spottrack.platform.profiles.domain.model.valueobjects.EmailAddress;
-import com.spottrack.platform.profiles.domain.model.valueobjects.PhoneNumber;
 
 public record CreateAdminCommand(
         Long userId,
         EmailAddress emailAddress,
         String firstName,
         String lastName,
-        PhoneNumber phoneNumber,
-        Dni dni
+        String phoneNumber,
+        String dni
 ) {}
