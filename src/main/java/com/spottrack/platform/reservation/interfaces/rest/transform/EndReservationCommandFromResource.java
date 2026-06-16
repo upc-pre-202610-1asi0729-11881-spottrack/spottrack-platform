c1ec777b45bdd00fecc6d3bfa232c43cd76f9e22
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-16 18:14:46 -0500

    feat(reservation): add end reservation command resource

---
+++ src/main/java/com/spottrack/platform/reservation/interfaces/rest/transform/EndReservationCommandFromResource.java
@@ -0,0 +1,12 @@
+package com.spottrack.platform.reservation.interfaces.rest.transform;
+
+import com.spottrack.platform.reservation.domain.model.commands.EndReservation;
+import com.spottrack.platform.reservation.domain.model.valueobjects.ReservationId;
+import com.spottrack.platform.reservation.interfaces.rest.resources.EndReservationCommandResource;
+
+public class EndReservationCommandFromResource {
+    public static EndReservation toCommandFromResource(EndReservationCommandResource resource) {
+        var command = new EndReservation(new ReservationId(resource.reservationId()));
+        return command;
+    }
+}
