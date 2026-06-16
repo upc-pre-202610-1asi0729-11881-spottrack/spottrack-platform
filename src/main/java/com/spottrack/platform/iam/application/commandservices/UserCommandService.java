24d3d00a6a1d0a1a9f79c21323e4ca866cbcd1f6
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 22:22:13 -0500

    feat(iam): add SignInCommand handler to UserCommandService

--- src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
+++ src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
@@ -1,10 +1,13 @@
 package com.spottrack.platform.iam.application.commandservices;
 
 import com.spottrack.platform.iam.domain.model.aggregates.User;
+import com.spottrack.platform.iam.domain.model.commands.SignInCommand;
 import com.spottrack.platform.iam.domain.model.commands.SignUpCommand;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 import com.spottrack.platform.shared.application.result.Result;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public interface UserCommandService {
     Result<User, ApplicationError> handle(SignUpCommand command);
+    Result<ImmutablePair<User, String>, ApplicationError> handle(SignInCommand command);
 }
