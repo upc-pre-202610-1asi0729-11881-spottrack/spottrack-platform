f2195d928390900622c8ec3afc9c51ea9a00a1c8
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-15 22:52:10 -0500

    feat(iam): add SignOutCommand handler to UserCommandService

--- src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
+++ src/main/java/com/spottrack/platform/iam/application/commandservices/UserCommandService.java
@@ -3,6 +3,7 @@ package com.spottrack.platform.iam.application.commandservices;
 import com.spottrack.platform.iam.domain.model.aggregates.User;
 import com.spottrack.platform.iam.domain.model.commands.ResetPasswordCommand;
 import com.spottrack.platform.iam.domain.model.commands.SignInCommand;
+import com.spottrack.platform.iam.domain.model.commands.SignOutCommand;
 import com.spottrack.platform.iam.domain.model.commands.SignUpCommand;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 import com.spottrack.platform.shared.application.result.Result;
@@ -12,4 +13,5 @@ public interface UserCommandService {
     Result<User, ApplicationError> handle(SignUpCommand command);
     Result<ImmutablePair<User, String>, ApplicationError> handle(SignInCommand command);
     Result<User, ApplicationError> handle(ResetPasswordCommand command);
+    Result<User, ApplicationError> handle(SignOutCommand command);
 }
