1e16c394d6c833af37bd773d71d2c4f481a8533f
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-18 18:45:30 -0500

    feat(maintenance): add maintenance persistence entity

--- src/main/java/com/spottrack/platform/maintenance/domain/model/aggregates/Maintenance.java
+++ src/main/java/com/spottrack/platform/maintenance/domain/model/aggregates/Maintenance.java
@@ -12,11 +12,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter
-@Entity
+@Setter
 public class Maintenance extends AbstractDomainAggregateRoot<Maintenance> {
 
     private MaintenanceId id;
