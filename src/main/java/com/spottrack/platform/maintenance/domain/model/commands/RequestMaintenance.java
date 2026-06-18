be50347aa12692149726646e54e1d6f80ea7c82f
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-18 16:49:15 -0500

    refactor(maintenance): modify request-maintenance command to work with equipment Id VO

--- src/main/java/com/spottrack/platform/maintenance/domain/model/commands/RequestMaintenance.java
+++ src/main/java/com/spottrack/platform/maintenance/domain/model/commands/RequestMaintenance.java
@@ -1,13 +1,13 @@
 package com.spottrack.platform.maintenance.domain.model.commands;
 
+import com.spottrack.platform.reservation.domain.model.valueobjects.EquipmentId;
+
 public record RequestMaintenance(
-        String equipmentId,
+        EquipmentId equipmentId,
         String requestedBy,
         String description
 ) {
     public RequestMaintenance {
-        if (equipmentId == null || equipmentId.isBlank())
-            throw new IllegalArgumentException("maintenance.command.requestMaintenance.equipmentId.notBlank");
         if (requestedBy == null || requestedBy.isBlank())
             throw new IllegalArgumentException("maintenance.command.requestMaintenance.requestedBy.notBlank");
         if (description == null || description.isBlank())
