bcca3e0fde05f4942056c03ac525e005cc9e5d3b
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-18 21:53:46 -0500

    fix(maintenance): fix findByStatus field name in MaintenancePersistenceRepository

--- src/main/java/com/spottrack/platform/maintenance/infrastructure/persistence/jpa/repositories/MaintenancePersistenceRepository.java
+++ src/main/java/com/spottrack/platform/maintenance/infrastructure/persistence/jpa/repositories/MaintenancePersistenceRepository.java
@@ -1,15 +1,11 @@
 package com.spottrack.platform.maintenance.infrastructure.persistence.jpa.repositories;
 
-import com.spottrack.platform.maintenance.domain.model.aggregates.Maintenance;
-import com.spottrack.platform.maintenance.domain.model.valueobjects.MaintenanceId;
 import com.spottrack.platform.maintenance.infrastructure.persistence.jpa.entities.MaintenancePersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MaintenancePersistenceRepository extends JpaRepository<MaintenancePersistenceEntity, Long> {
-    Optional<MaintenancePersistenceEntity> findByMaintenanceId(String id);
-    Optional<MaintenancePersistenceEntity> findByMaintenanceStatus(String status);
-    List<MaintenancePersistenceEntity> findAll();
+    Optional<MaintenancePersistenceEntity> findByMaintenanceId(String maintenanceId);
+    Optional<MaintenancePersistenceEntity> findByStatus(String status);
 }
