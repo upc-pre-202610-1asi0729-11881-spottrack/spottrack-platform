13adbc15acdccbc6d51b95389387f42eb239255a
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-20 12:11:29 -0500

    feat(monitoring): add sessionTrackerPersistenceRepository

--- src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/SessionTrackerPersistenceRepository.java
+++ src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/SessionTrackerPersistenceRepository.java
@@ -1,6 +1,11 @@
 package com.spottrack.platform.monitoring.infrastructure.persistence.jpa.repositories;
 
+import com.spottrack.platform.monitoring.infrastructure.persistence.jpa.entities.SessionTrackerPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SessionTrackerPersistenceRepository extends JpaRepository<> {
+import java.util.Optional;
+
+public interface SessionTrackerPersistenceRepository extends JpaRepository<SessionTrackerPersistenceEntity, Long> {
+    Optional<SessionTrackerPersistenceEntity> findBySessionTrackerId(String sessionTrackerId);
+    Optional<SessionTrackerPersistenceEntity> findByReservationId(String reservationId);
 }
