0892a48facbb90ace07c6a51a28394ed0b74bdd0
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-20 18:04:33 -0500

    feat(monitoring): add createSessionTrackerController POST method added

--- src/main/java/com/spottrack/platform/monitoring/interfaces/rest/SessionTrackerController.java
+++ src/main/java/com/spottrack/platform/monitoring/interfaces/rest/SessionTrackerController.java
@@ -1,7 +1,13 @@
 package com.spottrack.platform.monitoring.interfaces.rest;
 
 import com.spottrack.platform.monitoring.application.commandServices.SessionTrackerCommandService;
+import com.spottrack.platform.monitoring.domain.model.aggregates.SessionTracker;
+import com.spottrack.platform.monitoring.infrastructure.persistence.jpa.assemblers.SessionTrackerPersistenceAssembler;
 import com.spottrack.platform.monitoring.interfaces.rest.resources.CreateSessionTrackerResource;
+import com.spottrack.platform.monitoring.interfaces.rest.transform.CreateSessionTrackerCommandFromResource;
+import com.spottrack.platform.shared.application.result.ApplicationError;
+import com.spottrack.platform.shared.application.result.Result;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +24,15 @@ public class SessionTrackerController {
 
     @PostMapping("/sessionTracker/create")
     public ResponseEntity createSessionTracker(@RequestBody CreateSessionTrackerResource resource){
-        try {
+            var command = CreateSessionTrackerCommandFromResource.toCommandFromResource(resource);
+            var result = sessionTrackerCommandService.handle(command);
+            return switch(result){
+                case Result.Success< SessionTracker, ApplicationError > s->
+                    ResponseEntity.status(HttpStatus.CREATED).body(SessionTrackerPersistenceAssembler.toPersistenceFromDomain(s.value()));
 
-        }
+                case Result.Failure<SessionTracker, ApplicationError> f->
+                    ResponseEntity.badRequest().body(f.error());
+            };
     }
 
 }
