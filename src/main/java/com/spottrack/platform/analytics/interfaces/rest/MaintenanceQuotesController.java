1d7448970108a1b3f5d8ccf5f93d43727d5d835c
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 03:00:12 -0500

    fix(analytics): inject MaintenanceQuoteCommandService interface in controller

--- src/main/java/com/spottrack/platform/analytics/interfaces/rest/MaintenanceQuotesController.java
+++ src/main/java/com/spottrack/platform/analytics/interfaces/rest/MaintenanceQuotesController.java
@@ -1,24 +1,26 @@
 package com.spottrack.platform.analytics.interfaces.rest;
 
-import com.spottrack.platform.analytics.application.internal.commandservices.MaintenanceQuoteCommandServiceImpl;
+import com.spottrack.platform.analytics.application.commandservices.MaintenanceQuoteCommandService;
+import com.spottrack.platform.analytics.domain.model.commands.RequestADetailedMaintenanceQuoteCommand;
 import com.spottrack.platform.analytics.interfaces.rest.resources.MaintenanceQuoteResource;
 import com.spottrack.platform.analytics.interfaces.rest.transform.MaintenanceQuoteResourceFromEntityAssembler;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/maintenance-quotes")
+@RequestMapping(value = "/api/v1/maintenance-quotes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MaintenanceQuotesController {
-    private final MaintenanceQuoteCommandServiceImpl maintenanceQuoteCommandService;
+    private final MaintenanceQuoteCommandService maintenanceQuoteCommandService;
 
-    public MaintenanceQuotesController(MaintenanceQuoteCommandServiceImpl maintenanceQuoteCommandService) {
+    public MaintenanceQuotesController(MaintenanceQuoteCommandService maintenanceQuoteCommandService) {
         this.maintenanceQuoteCommandService = maintenanceQuoteCommandService;
     }
 
     @PostMapping
-    public ResponseEntity<MaintenanceQuoteResource> createQuote() {
-        var quote = maintenanceQuoteCommandService.handle(1L);
+    public ResponseEntity<MaintenanceQuoteResource> createQuote(@RequestBody RequestADetailedMaintenanceQuoteCommand command) {
+        var quote = maintenanceQuoteCommandService.handle(command);
         return quote.map(value -> new ResponseEntity<>(MaintenanceQuoteResourceFromEntityAssembler.toResourceFromEntity(value), HttpStatus.CREATED))
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
