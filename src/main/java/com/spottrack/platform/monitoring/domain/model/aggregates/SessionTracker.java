370d6cef51bfdc2f3ef52869beb67468faeaad6d
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-20 11:54:11 -0500

    feat(monitoring): created SessionTracker aggregate, not completerd yet

--- src/main/java/com/spottrack/platform/monitoring/domain/model/aggregates/SessionTracker.java
+++ src/main/java/com/spottrack/platform/monitoring/domain/model/aggregates/SessionTracker.java
@@ -1,8 +1,19 @@
 package com.spottrack.platform.monitoring.domain.model.aggregates;
 
+import com.spottrack.platform.monitoring.domain.model.valueObjects.ReservationId;
 import com.spottrack.platform.monitoring.domain.model.valueObjects.UsageActivity;
 
 public class SessionTracker {
     UsageActivity activity;
+    /**
+     * id of the reservation that the session tracker is monitoring
+     */
+    ReservationId reservationId;
+    boolean sessionIsInactive;
+    boolean sessionIsActive;
+
+    public SessionTracker(){
+
+    }
 
 }
