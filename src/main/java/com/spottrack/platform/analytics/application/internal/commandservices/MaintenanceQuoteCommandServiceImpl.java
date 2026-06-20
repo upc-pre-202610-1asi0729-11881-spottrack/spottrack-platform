5c2b477c22c056b3af818f9bfb2430a19dc4dbd5
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 02:59:43 -0500

    fix(analytics): implement MaintenanceQuoteCommandService in Impl

--- src/main/java/com/spottrack/platform/analytics/application/internal/commandservices/MaintenanceQuoteCommandServiceImpl.java
+++ src/main/java/com/spottrack/platform/analytics/application/internal/commandservices/MaintenanceQuoteCommandServiceImpl.java
@@ -1,6 +1,8 @@
 package com.spottrack.platform.analytics.application.internal.commandservices;
 
+import com.spottrack.platform.analytics.application.commandservices.MaintenanceQuoteCommandService;
 import com.spottrack.platform.analytics.domain.model.aggregates.MaintenanceQuote;
+import com.spottrack.platform.analytics.domain.model.commands.RequestADetailedMaintenanceQuoteCommand;
 import com.spottrack.platform.analytics.domain.model.valueobjects.MaintenanceQuoteId;
 import com.spottrack.platform.analytics.domain.repositories.MaintenanceQuoteRepository;
 import org.springframework.stereotype.Service;
@@ -8,7 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
-public class MaintenanceQuoteCommandServiceImpl {
+public class MaintenanceQuoteCommandServiceImpl implements MaintenanceQuoteCommandService {
 
     private final MaintenanceQuoteRepository maintenanceQuoteRepository;
 
@@ -16,11 +18,10 @@ public class MaintenanceQuoteCommandServiceImpl {
         this.maintenanceQuoteRepository = maintenanceQuoteRepository;
     }
 
-    // Método para procesar la creación de una cotización de mantenimiento
-    public Optional<MaintenanceQuote> handle(Long id) {
-        var maintenanceQuoteId = new MaintenanceQuoteId(ThreadLocalRandom.current().nextLong(1000, 100000));
+    public Optional<MaintenanceQuote> handle(RequestADetailedMaintenanceQuoteCommand command) {
+        var maintenanceQuoteId = new MaintenanceQuoteId(
+            ThreadLocalRandom.current().nextLong(1000, 100000));
         var maintenanceQuote = new MaintenanceQuote(maintenanceQuoteId);
-
         this.maintenanceQuoteRepository.save(maintenanceQuote);
         return Optional.of(maintenanceQuote);
     }
