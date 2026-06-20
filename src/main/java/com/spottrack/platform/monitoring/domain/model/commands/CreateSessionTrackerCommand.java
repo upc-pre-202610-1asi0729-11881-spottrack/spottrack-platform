cb91000f75072375a66ad6a7f388f4e89de88249
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-20 11:58:05 -0500

    feat(monitoring): add CreateSessionTrackerComand

--- src/main/java/com/spottrack/platform/monitoring/domain/model/commands/CreateSessionTrackerCommand.java
+++ src/main/java/com/spottrack/platform/monitoring/domain/model/commands/CreateSessionTrackerCommand.java
@@ -1,4 +1,8 @@
 package com.spottrack.platform.monitoring.domain.model.commands;
 
-public record CreateSessionTrackerCommand() {
+import com.spottrack.platform.monitoring.domain.model.valueObjects.ReservationId;
+import com.spottrack.platform.monitoring.domain.model.valueObjects.SessionTrackerId;
+import com.spottrack.platform.monitoring.domain.model.valueObjects.UsageActivity;
+
+public record CreateSessionTrackerCommand(SessionTrackerId sessionTrackerId, ReservationId reservationId, boolean sessionIsActive, boolean sessionIsInactive, UsageActivity usageActivity) {
 }
