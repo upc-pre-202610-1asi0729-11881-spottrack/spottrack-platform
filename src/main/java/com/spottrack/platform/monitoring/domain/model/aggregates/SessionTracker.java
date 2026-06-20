5af160f8c03b5d2d915bd59c9da040eeb47b5be9
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-20 11:59:22 -0500

    feat(monitoring): add constructor in SessionTracker

--- src/main/java/com/spottrack/platform/monitoring/domain/model/aggregates/SessionTracker.java
+++ src/main/java/com/spottrack/platform/monitoring/domain/model/aggregates/SessionTracker.java
@@ -1,5 +1,6 @@
 package com.spottrack.platform.monitoring.domain.model.aggregates;
 
+import com.spottrack.platform.monitoring.domain.model.commands.CreateSessionTrackerCommand;
 import com.spottrack.platform.monitoring.domain.model.valueObjects.ReservationId;
 import com.spottrack.platform.monitoring.domain.model.valueObjects.UsageActivity;
 
@@ -7,9 +8,7 @@ public class SessionTracker {
     /**
      * For security measures, we will use uuids as secondary Ids aside from the real DB Long Ids
      */
-
-
-    UsageActivity activity;
+    UsageActivity usageActivity;
     /**
      * id of the reservation that the session tracker is monitoring
      */
@@ -17,8 +16,11 @@ public class SessionTracker {
     boolean sessionIsInactive;
     boolean sessionIsActive;
 
-    public SessionTracker(){
-
+    public SessionTracker(CreateSessionTrackerCommand command){
+        this.usageActivity = command.usageActivity();
+        this.reservationId = command.reservationId();
+        this.sessionIsInactive = command.sessionIsInactive();
+        this.sessionIsActive = command.sessionIsActive();
     }
 
 }
