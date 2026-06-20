898f6cf50bae9e8c77a539d21a2b04432f77138b
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 05:43:43 -0500

    feat(shared): add global CORS configuration

---
+++ src/main/java/com/spottrack/platform/shared/infrastructure/cors/WebConfig.java
@@ -0,0 +1,20 @@
+package com.spottrack.platform.shared.infrastructure.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                    "http://localhost:4200",
+                    "https://lively-ground-08011af0f.7.azurestaticapps.net"
+                )
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
