bf52b2304a30330d9e0806277ca2666ba46ac4cb
Llummo <>
2026-06-22 08:19:33 -0500

    feat(monitoring): add equipment-id in monitoring

--- src/main/java/com/spottrack/platform/monitoring/domain/model/entities/Report.java
+++ src/main/java/com/spottrack/platform/monitoring/domain/model/entities/Report.java
@@ -1,6 +1,12 @@
 package com.spottrack.platform.monitoring.domain.model.entities;
 
+import com.spottrack.platform.monitoring.domain.model.valueobjects.ReportId;
+
+import java.time.LocalTime;
+
+
 public class Report {
-    Long Id;
-    
+    ReportId reportId;
+    LocalTime detectionStamp;
+
 }
