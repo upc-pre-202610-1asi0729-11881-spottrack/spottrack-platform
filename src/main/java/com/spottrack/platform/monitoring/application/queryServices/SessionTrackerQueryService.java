fb35ca2434d0cfe547adfd70216b8094876b9f71
ImaLi06 <imanollimache@gmail.com>
2026-06-25 12:44:27 -0500

    feat(monitoring): add GetActiveSessionTrackersQuery to SessionTrackerQueryService

--- src/main/java/com/spottrack/platform/monitoring/application/queryServices/SessionTrackerQueryService.java
+++ src/main/java/com/spottrack/platform/monitoring/application/queryServices/SessionTrackerQueryService.java
@@ -1,12 +1,16 @@
 package com.spottrack.platform.monitoring.application.queryServices;
 
 import com.spottrack.platform.monitoring.domain.model.aggregates.SessionTracker;
+import com.spottrack.platform.monitoring.domain.model.queries.GetActiveSessionTrackersQuery;
 import com.spottrack.platform.monitoring.domain.model.queries.GetSessionTrackerByIdQuery;
 import com.spottrack.platform.shared.application.result.ApplicationError;
+import com.spottrack.platform.shared.application.result.Result;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SessionTrackerQueryService {
+    Result<List<SessionTracker>, ApplicationError> handle(GetActiveSessionTrackersQuery query);
     Optional<SessionTracker> handle(GetSessionTrackerByIdQuery query);
 
 }
