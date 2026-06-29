5c23824a7966b862bd44f248dd5316a5b3d259e7
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-28 23:29:34 -0500

    feat(membership): add membershipType enum

--- src/main/java/com/spottrack/platform/membership/domain/model/aggregates/Membership.java
+++ src/main/java/com/spottrack/platform/membership/domain/model/aggregates/Membership.java
@@ -1,11 +1,14 @@
 package com.spottrack.platform.membership.domain.model.aggregates;
 
+import com.spottrack.platform.membership.domain.model.valueobjects.MembershipId;
 import com.spottrack.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
 
 public class Membership extends AbstractDomainAggregateRoot<Membership> {
 
-    Long id;
-    String uuid;
+    Long id; // persistenceId
+    MembershipId membershipId; // DomainId
+
+
 
     public Membership() {
 
---
+++ src/main/java/com/spottrack/platform/membership/domain/model/valueobjects/MembershipType.java
@@ -0,0 +1,7 @@
+package com.spottrack.platform.membership.domain.model.valueobjects;
+
+public enum MembershipType {
+    BASIC,
+    MID,
+    PLATINUM
+}
