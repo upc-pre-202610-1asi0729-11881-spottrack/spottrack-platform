a55258bfd938748925337af7c8466d76ad1cf2ce
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-30 04:13:09 -0500

    feat(profiles): add handle(GetClientByUserIdQuery) to ClientQueryService interface

--- src/main/java/com/spottrack/platform/profiles/application/queryservices/ClientQueryService.java
+++ src/main/java/com/spottrack/platform/profiles/application/queryservices/ClientQueryService.java
@@ -3,10 +3,12 @@ package com.spottrack.platform.profiles.application.queryservices;
 import com.spottrack.platform.profiles.domain.model.aggregates.Client;
 import com.spottrack.platform.profiles.domain.model.queries.GetClientByEmailQuery;
 import com.spottrack.platform.profiles.domain.model.queries.GetClientByIdQuery;
+import com.spottrack.platform.profiles.domain.model.queries.GetClientByUserIdQuery;
 
 import java.util.Optional;
 
 public interface ClientQueryService {
     Optional<Client> handle(GetClientByIdQuery query);
+    Optional<Client> handle(GetClientByUserIdQuery query);
     Optional<Client> handle(GetClientByEmailQuery query);
 }
