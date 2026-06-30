a26742b88c5bbac6df7e38f755d88c729430aa45
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-30 05:15:21 -0500

    feat(reservations): extend ReservationQueryService with uuid and clientId lookups

--- src/main/java/com/spottrack/platform/reservation/application/queryservices/ReservationQueryService.java
+++ src/main/java/com/spottrack/platform/reservation/application/queryservices/ReservationQueryService.java
@@ -2,11 +2,16 @@ package com.spottrack.platform.reservation.application.queryservices;
 
 import com.spottrack.platform.reservation.domain.model.aggregates.Reservation;
 import com.spottrack.platform.reservation.domain.model.queries.GetAllReservationsQuery;
+import com.spottrack.platform.reservation.domain.model.queries.GetReservationByUuidQuery;
+import com.spottrack.platform.reservation.domain.model.queries.GetReservationsByClientIdQuery;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface ReservationQueryService {
     List<Reservation> handle(GetAllReservationsQuery query);
+    Optional<Reservation> handle(GetReservationByUuidQuery query);
+    List<Reservation> handle(GetReservationsByClientIdQuery query);
 }
