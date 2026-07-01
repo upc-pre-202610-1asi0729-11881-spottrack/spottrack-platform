94b7ef99b081063c4009c74eebd4c58c63db2bd9
ImaLi06 <imanollimache@gmail.com>
2026-07-01 15:52:42 -0500

    feat(monitoring): add findBySessionTrackerId to AnomalyReportPersistenceRepository

--- src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/AnomalyReportPersistenceRepository.java
+++ src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/AnomalyReportPersistenceRepository.java
@@ -4,9 +4,11 @@ import com.spottrack.platform.monitoring.infrastructure.persistence.jpa.entities
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AnomalyReportPersistenceRepository extends JpaRepository<AnomalyReportPersistenceEntity, Long> {
     Optional<AnomalyReportPersistenceEntity> findByAnomalyReportId(String anomalyReportId);
+    List<AnomalyReportPersistenceEntity> findBySessionTrackerId(String sessionTrackerId);
 }
