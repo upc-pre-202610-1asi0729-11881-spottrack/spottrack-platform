b4049e0e3013974f02c5d37133b3e567d217cb8c
Llummo <167897805+Llummo@users.noreply.github.com>
2026-07-01 14:22:08 -0500

    feat(monitoring): add  ReportAnomalyCommand impl

--- src/main/java/com/spottrack/platform/monitoring/application/internal/commandcervices/AnomalyCommandServiceImplementation.java
+++ src/main/java/com/spottrack/platform/monitoring/application/internal/commandcervices/AnomalyCommandServiceImplementation.java
@@ -3,14 +3,30 @@ package com.spottrack.platform.monitoring.application.internal.commandcervices;
 import com.spottrack.platform.monitoring.application.commandServices.AnomalyCommandService;
 import com.spottrack.platform.monitoring.domain.model.aggregates.Anomaly;
 import com.spottrack.platform.monitoring.domain.model.commands.ReportAnomalyCommand;
+import com.spottrack.platform.monitoring.domain.repositories.AnomalyRepository;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 import com.spottrack.platform.shared.application.result.Result;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnomalyCommandServiceImplementation implements AnomalyCommandService {
+    private final AnomalyRepository anomalyRepository;
+    public AnomalyCommandServiceImplementation(AnomalyRepository anomalyRepository){
+        this.anomalyRepository = anomalyRepository;
+    }
+
     @Override
     public Result<Anomaly, ApplicationError> handle(ReportAnomalyCommand command) {
-        return null;
+        try {
+            var anomaly = new Anomaly(command);
+            var saved = anomalyRepository.save(anomaly);
+            return Result.success(saved);
+        } catch(IllegalArgumentException e ){
+            return Result.failure(ApplicationError.validationError("Anomaly report", e.getMessage()));
+        }
+        catch (Exception e) {
+            return Result.failure(ApplicationError.unexpected("Anomaly report", e.getMessage()));
+        }
+
     }
 }
