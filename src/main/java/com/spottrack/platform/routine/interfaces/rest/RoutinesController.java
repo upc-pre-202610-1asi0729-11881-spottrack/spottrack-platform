6b898ccde06e3ed3a3001b0d71951a9344a5d591
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-02 11:04:15 -0500

    feat(routines): replace checkMembershipAccess with GymMembershipAccessGuard in RoutinesController

--- src/main/java/com/spottrack/platform/routine/interfaces/rest/RoutinesController.java
+++ src/main/java/com/spottrack/platform/routine/interfaces/rest/RoutinesController.java
@@ -1,8 +1,8 @@
 package com.spottrack.platform.routine.interfaces.rest;
 
 import com.spottrack.platform.iam.interfaces.acl.IamContextFacade;
-import com.spottrack.platform.membership.interfaces.acl.MembershipContextFacade;
 import com.spottrack.platform.profiles.interfaces.acl.ProfilesContextFacade;
+import com.spottrack.platform.shared.interfaces.rest.guards.GymMembershipAccessGuard;
 import com.spottrack.platform.routine.application.commandservices.RoutineCommandService;
 import com.spottrack.platform.routine.application.queryservices.RoutineQueryService;
 import com.spottrack.platform.routine.domain.model.queries.GetAllRoutinesByClientIdQuery;
@@ -45,19 +45,19 @@ public class RoutinesController {
     private final RoutineQueryService routineQueryService;
     private final IamContextFacade iamContextFacade;
     private final ProfilesContextFacade profilesContextFacade;
-    private final MembershipContextFacade membershipContextFacade;
+    private final GymMembershipAccessGuard gymMembershipAccessGuard;
 
     public RoutinesController(
             RoutineCommandService routineCommandService,
             RoutineQueryService routineQueryService,
             IamContextFacade iamContextFacade,
             ProfilesContextFacade profilesContextFacade,
-            MembershipContextFacade membershipContextFacade) {
+            GymMembershipAccessGuard gymMembershipAccessGuard) {
         this.routineCommandService = routineCommandService;
         this.routineQueryService = routineQueryService;
         this.iamContextFacade = iamContextFacade;
         this.profilesContextFacade = profilesContextFacade;
-        this.membershipContextFacade = membershipContextFacade;
+        this.gymMembershipAccessGuard = gymMembershipAccessGuard;
     }
 
     @PostMapping
@@ -82,7 +82,7 @@ public class RoutinesController {
             return ErrorResponseAssembler.toErrorResponseFromApplicationError(
                     ApplicationError.notFound("Client", authentication.getName()));
         }
-        var membershipError = checkMembershipAccess(clientId);
+        var membershipError = gymMembershipAccessGuard.check(clientId);
         if (membershipError.isPresent()) return membershipError.get();
         var command = CreateRoutineCommandFromResourceAssembler.toCommandFromResource(resource, clientId);
         var result = routineCommandService.handle(command);
@@ -200,16 +200,6 @@ public class RoutinesController {
                 ExerciseBlockResourceFromEntityAssembler::toResourceFromEntity,
                 HttpStatus.CREATED
         );
-    }
-
-    private Optional<ResponseEntity<?>> checkMembershipAccess(Long clientId) {
-        var accessStatus = membershipContextFacade.fetchMembershipAccessStatus(clientId);
-        if ("ACTIVE".equals(accessStatus)) return Optional.empty();
-        var errorCode = "SUSPENDED".equals(accessStatus)
-                ? "membership.error.access.suspended"
-                : "membership.error.access.inactive";
-        return Optional.of(ErrorResponseAssembler.toErrorResponseFromApplicationError(
-                ApplicationError.forbidden("Membership", errorCode)));
     }
 
     private Long resolveClientId(Authentication authentication) {
