8322418f235725d56cbd7eb1f8e5179d1650b27b
ImaLi06 <imanollimache@gmail.com>
2026-07-02 02:29:20 -0500

    feat(maintenance): update CreateTechnicalticketResource attributes

--- src/main/java/com/spottrack/platform/maintenance/interfaces/rest/resources/TechnicalTicketResource.java
+++ src/main/java/com/spottrack/platform/maintenance/interfaces/rest/resources/TechnicalTicketResource.java
@@ -1,7 +1,9 @@
 package com.spottrack.platform.maintenance.interfaces.rest.resources;
 
 import com.spottrack.platform.maintenance.domain.model.valueobjects.MaintenanceStatus;
+import com.spottrack.platform.maintenance.domain.model.valueobjects.TicketPriority;
 import com.spottrack.platform.maintenance.domain.model.valueobjects.TicketStatus;
+import com.spottrack.platform.maintenance.domain.model.valueobjects.TicketType;
 
 public record TechnicalTicketResource(
         String id,
@@ -9,6 +11,8 @@ public record TechnicalTicketResource(
         String equipmentId,
         String technicianId,
         String description,
+        TicketPriority priority,
+        TicketType type,
         TicketStatus ticketStatus,
         MaintenanceStatus maintenanceStatus
 ) {}
--- src/main/java/com/spottrack/platform/maintenance/interfaces/rest/transform/CreateTechnicalTicketCommandFromResourceAssembler.java
+++ src/main/java/com/spottrack/platform/maintenance/interfaces/rest/transform/CreateTechnicalTicketCommandFromResourceAssembler.java
@@ -1,16 +1,18 @@
 package com.spottrack.platform.maintenance.interfaces.rest.transform;
 
-import com.spottrack.platform.maintenance.domain.model.commands.CreateTechnicalTicket;
-import com.spottrack.platform.maintenance.domain.model.valueobjects.MaintenanceId;
+import com.spottrack.platform.maintenance.domain.model.commands.CreateTechnicalTicketCommand;
+import com.spottrack.platform.maintenance.domain.model.valueobjects.TicketPriority;
+import com.spottrack.platform.maintenance.domain.model.valueobjects.TicketType;
 import com.spottrack.platform.maintenance.interfaces.rest.resources.CreateTechnicalTicketResource;
 
 public class CreateTechnicalTicketCommandFromResourceAssembler {
 
-    public static CreateTechnicalTicket toCommandFromResource(CreateTechnicalTicketResource resource) {
-        return new CreateTechnicalTicket(
-                new MaintenanceId(resource.maintenanceId()),
+    public static CreateTechnicalTicketCommand toCommandFromResource(CreateTechnicalTicketResource resource) {
+        return new CreateTechnicalTicketCommand(
                 resource.equipmentId(),
-                resource.description()
+                resource.description(),
+                TicketPriority.valueOf(resource.priority()),
+                TicketType.valueOf(resource.type())
         );
     }
 }
