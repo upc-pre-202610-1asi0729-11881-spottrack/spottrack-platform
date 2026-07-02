5a868d084f659c322c3b78de753980944bb13e71
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-02 01:59:08 -0500

    feat(membership): add PaymentPurpose enum

---
+++ src/main/java/com/spottrack/platform/membership/domain/model/valueobjects/PaymentPurpose.java
@@ -0,0 +1,8 @@
+package com.spottrack.platform.membership.domain.model.valueobjects;
+
+public enum PaymentPurpose {
+    NEW_MEMBERSHIP,
+    BUSINESS_REGISTRATION,
+    DEBT_RENEWAL,
+    PLAN_UPGRADE
+}
