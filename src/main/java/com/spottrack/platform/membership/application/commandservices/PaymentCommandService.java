4030ae2142be30e64d0e69c45726a1cf471c06d7
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-02 02:00:44 -0500

    feat(membership): add handle(InitiateUpgradePaymentCommand) to PaymentCommandService

--- src/main/java/com/spottrack/platform/membership/application/commandservices/PaymentCommandService.java
+++ src/main/java/com/spottrack/platform/membership/application/commandservices/PaymentCommandService.java
@@ -5,6 +5,7 @@ import com.spottrack.platform.membership.domain.model.commands.ConfirmPaymentCom
 import com.spottrack.platform.membership.domain.model.commands.FailPaymentCommand;
 import com.spottrack.platform.membership.domain.model.commands.InitiateBusinessPaymentCommand;
 import com.spottrack.platform.membership.domain.model.commands.InitiateDebtPaymentCommand;
+import com.spottrack.platform.membership.domain.model.commands.InitiateUpgradePaymentCommand;
 import com.spottrack.platform.membership.domain.model.commands.PayMembershipCommand;
 import com.spottrack.platform.shared.application.result.ApplicationError;
 import com.spottrack.platform.shared.application.result.Result;
@@ -13,6 +14,7 @@ public interface PaymentCommandService {
     Result<String, ApplicationError> handle(PayMembershipCommand command);
     Result<String, ApplicationError> handle(InitiateBusinessPaymentCommand command);
     Result<String, ApplicationError> handle(InitiateDebtPaymentCommand command);
+    Result<String, ApplicationError> handle(InitiateUpgradePaymentCommand command);
     Result<Payment, ApplicationError> handle(ConfirmPaymentCommand command);
     Result<Payment, ApplicationError> handle(FailPaymentCommand command);
 }
