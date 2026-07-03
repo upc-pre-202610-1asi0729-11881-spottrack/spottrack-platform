a0a7c6d4c5d6a562470509f95fdd0b9e6e0f8a5a
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-03 07:38:07 -0500

    feat(gym): add gymId field to Branch entity

--- src/main/java/com/spottrack/platform/gym/domain/model/entities/Branch.java
+++ src/main/java/com/spottrack/platform/gym/domain/model/entities/Branch.java
@@ -12,16 +12,19 @@ import java.util.UUID;
 public class Branch {
 
     private final BranchId id;
+    private String gymId;
     private String name;
     private String address;
 
     private List<Zone> zoneList;
+
     protected Branch() {
         this.id = null;
     }
 
-    public Branch(String name, String address) {
+    public Branch(String gymId, String name, String address) {
         this.id = new BranchId(UUID.randomUUID().toString());
+        this.gymId = gymId;
         this.name = name;
         this.address = address;
         this.zoneList = new ArrayList<Zone>();
