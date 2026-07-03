fa4d84ae64fee73961c456441b8a0a3cc9771cfe
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-02 21:14:15 -0500

    feat(membership): add handle(GetPrimaryMembershipByClientIdQuery) to MembershipQueryService

--- src/main/java/com/spottrack/platform/membership/application/queryservices/MembershipQueryService.java
+++ src/main/java/com/spottrack/platform/membership/application/queryservices/MembershipQueryService.java
@@ -3,6 +3,7 @@ package com.spottrack.platform.membership.application.queryservices;
 import com.spottrack.platform.membership.domain.model.aggregates.Membership;
 import com.spottrack.platform.membership.domain.model.queries.GetMembershipByIdQuery;
 import com.spottrack.platform.membership.domain.model.queries.GetMembershipsByClientIdQuery;
+import com.spottrack.platform.membership.domain.model.queries.GetPrimaryMembershipByClientIdQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface MembershipQueryService {
     Optional<Membership> handle(GetMembershipByIdQuery query);
     List<Membership> handle(GetMembershipsByClientIdQuery query);
+    Optional<Membership> handle(GetPrimaryMembershipByClientIdQuery query);
 }
