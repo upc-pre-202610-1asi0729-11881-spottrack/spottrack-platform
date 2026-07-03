7131bc7a7edf326c34f297f9987b3c740735e307
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-02 20:06:27 -0500

    feat(gym): add handle(GetAllGymsQuery) to GymQueryService

--- src/main/java/com/spottrack/platform/gym/application/queryservices/GymQueryService.java
+++ src/main/java/com/spottrack/platform/gym/application/queryservices/GymQueryService.java
@@ -2,6 +2,7 @@ package com.spottrack.platform.gym.application.queryservices;
 
 import com.spottrack.platform.gym.domain.model.aggregates.Gym;
 import com.spottrack.platform.gym.domain.model.entities.GymWhitelistEntry;
+import com.spottrack.platform.gym.domain.model.queries.GetAllGymsQuery;
 import com.spottrack.platform.gym.domain.model.queries.GetGymById;
 import com.spottrack.platform.gym.domain.model.queries.GetGymsByAdminUserId;
 import com.spottrack.platform.gym.domain.model.queries.GetWhitelistByGymIdQuery;
@@ -12,5 +13,6 @@ import java.util.Optional;
 public interface GymQueryService {
     Optional<Gym> handle(GetGymById query);
     List<Gym> handle(GetGymsByAdminUserId query);
+    List<Gym> handle(GetAllGymsQuery query);
     List<GymWhitelistEntry> handle(GetWhitelistByGymIdQuery query);
 }
