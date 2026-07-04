1bc84dd464454fad927341c39dba3fa4372ef56f
ImaLi06 <imanollimache@gmail.com>
2026-07-04 09:29:13 -0500

    feat(monitoring): add queries for monitoring Motion sensor connection

--- src/main/java/com/spottrack/platform/monitoring/domain/repositories/MotionSensorRepository.java
+++ src/main/java/com/spottrack/platform/monitoring/domain/repositories/MotionSensorRepository.java
@@ -4,11 +4,16 @@ import com.spottrack.platform.monitoring.domain.model.aggregates.MotionSensor;
 import com.spottrack.platform.monitoring.domain.model.valueobjects.EquipmentId;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MotionSensorRepository {
     boolean existsByEquipmentId(EquipmentId equipmentId);
     List<MotionSensor> findAll();
+    Optional<MotionSensor> findById(Long id);
+    List<MotionSensor> findAllOnline();
+    List<MotionSensor> findAllOfflineSince(LocalDateTime threshold);
     MotionSensor save(MotionSensor motionSensor);
 }
--- src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/MotionSensorPersistenceRepository.java
+++ src/main/java/com/spottrack/platform/monitoring/infrastructure/persistence/jpa/repositories/MotionSensorPersistenceRepository.java
@@ -5,7 +5,12 @@ import com.spottrack.platform.monitoring.infrastructure.persistence.jpa.entities
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface MotionSensorPersistenceRepository extends JpaRepository<MotionSensorPersistenceEntity, Long> {
     boolean existsByEquipmentId(EquipmentId equipmentId);
+    List<MotionSensorPersistenceEntity> findByOnlineTrue();
+    List<MotionSensorPersistenceEntity> findByOnlineFalseAndLastStatusChangeAtBefore(LocalDateTime threshold);
 }
