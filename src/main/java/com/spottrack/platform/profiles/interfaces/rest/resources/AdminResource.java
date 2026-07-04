09b279299826d97e94423a0232554f35e9090be9
ImaLi06 <imanollimache@gmail.com>
2026-07-03 21:11:48 -0500

    feat(profiles): add firstName, lastName and dni attributes to AdminResource

--- src/main/java/com/spottrack/platform/profiles/interfaces/rest/resources/AdminResource.java
+++ src/main/java/com/spottrack/platform/profiles/interfaces/rest/resources/AdminResource.java
@@ -19,6 +19,15 @@ public record AdminResource(
         String email,
 
         @Schema(description = "Admin phone number, null if the profile is incomplete", example = "123456789")
-        String phoneNumber
+        String phoneNumber,
+
+        @Schema(description = "Admin first name, null if the profile is incomplete", example = "John")
+        String firstName,
+
+        @Schema(description = "Admin last name, null if the profile is incomplete", example = "Smith")
+        String lastName,
+
+        @Schema(description = "Admin DNI, null if the profile is incomplete", example = "87654321")
+        String dni
 ) {
 }
