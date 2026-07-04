5667cb4c9610b314fb62b6761e518f255415ecb2
ImaLi06 <imanollimache@gmail.com>
2026-07-03 21:13:29 -0500

    feat(profiles): add phoneNumber, firstName, lastName and dni to ClientResourceFromEntityAssembler

--- src/main/java/com/spottrack/platform/profiles/interfaces/rest/transform/ClientResourceFromEntityAssembler.java
+++ src/main/java/com/spottrack/platform/profiles/interfaces/rest/transform/ClientResourceFromEntityAssembler.java
@@ -6,13 +6,18 @@ import com.spottrack.platform.profiles.interfaces.rest.resources.ClientResource;
 public class ClientResourceFromEntityAssembler {
 
     public static ClientResource toResourceFromEntity(Client entity) {
-        var phoneNumber = entity.getPersonInfo() != null
-                ? entity.getPersonInfo().phoneNumber().phoneNumber()
-                : null;
+        var personInfo = entity.getPersonInfo();
+        var phoneNumber = personInfo != null ? personInfo.phoneNumber().phoneNumber() : null;
+        var firstName   = personInfo != null ? personInfo.firstName() : null;
+        var lastName    = personInfo != null ? personInfo.lastName() : null;
+        var dni         = personInfo != null ? personInfo.dni().dni() : null;
         return new ClientResource(
                 entity.getId(),
                 entity.getFullName(),
                 entity.getEmailAddress(),
-                phoneNumber);
+                phoneNumber,
+                firstName,
+                lastName,
+                dni);
     }
 }
